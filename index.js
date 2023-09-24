620a636138671e6fa5c5d86bcc8091a8f25c4880
s4remi <s4remi@gmail.com>
2023-09-23 22:45:01 -0700

    adding a new design and js file for project

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 console.log("hi there!");
+/*
 import express from "express";
 const app = express();
 const port = 3030;
 app.get("/", (req, res) => {
   res.sendFile("sindex.html");
 });
-
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
+});
+*/ // script.js
+
+// Function to handle the click event
+function handleLinkClick(event) {
+  // Get the URL from the "data-url" attribute of the clicked element
+  const url = event.target.getAttribute("data-url");
+
+  // Open the URL in a new tab or window
+  window.open(url, "_blank");
+}
+
+// Add click event listeners to all elements with the "link" class
+const linkElements = document.querySelectorAll(".link");
+linkElements.forEach((element) => {
+  element.addEventListener("click", handleLinkClick);
 });
